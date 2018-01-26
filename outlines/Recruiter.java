ddb24fb10acf18e510ef155aee59f5f7c28cdf6b
Jiaqing Mo <jimo@calpoly.edu>
2018-01-26 15:32:15 -0800

    issue #42

---
+++ outlines/Recruiter.java
@@ -0,0 +1,60 @@
+
+/** Project: Simternship
+* 
+* @author Jiaqing Mo
+* @version 0.1
+* 
+*/
+
+public class Recruiter extends NPC
+{
+   private Company company;
+   private CareerFairBooth booth;
+
+   /**
+    * construct a recruiter 
+    * @param name the name of the recruiter
+    * @param booth the career fair booth of the recruiter
+    * @param company the company of the recruiter
+    */
+   public Recruiter(Name name, CareerFairBooth booth, Company company)
+   {
+      this.name = name;
+      this.booth = booth;
+      this.company = company;
+   }
+
+   /**
+    * change the company of the recruiter
+    * @param comapny the company of the recuiter
+    */
+   public void setCompany(Company company){
+      this.company = company;
+   }
+
+   /**
+    * return the company of the recruiter
+    */
+   public Company getCompany(){
+      return this.company;
+   }
+
+   /**
+    * change the career fair booth of the recruiter
+    * @param booth the career fair booth of the recruiter
+    */
+   public void setCareerFairBooth(CareerFairBooth booth)
+   {
+      this.booth = booth;
+   }   
+
+   /**
+    * return the career fair booth of the recruiter
+    */
+   public CareerFairBooth getCareerFairBooth()
+   {
+      return this.booth;
+   }   
+
+}
+
