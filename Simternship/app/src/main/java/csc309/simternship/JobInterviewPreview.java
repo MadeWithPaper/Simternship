74b4e0ecf4a5bc3a85b9ebfccf347739e46c61be
dziermanfelix <dziermanfelix@gmail.com>
2018-01-31 20:06:47 -0800

    Dustyn Activities w/out event handling

--- Simternship/app/src/main/java/csc309/simternship/JobInterviewPreview.java
+++ Simternship/app/src/main/java/csc309/simternship/JobInterviewPreview.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class EndScreen extends AppCompatActivity {
+public class JobInterviewPreview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_end_screen);
+        setContentView(R.layout.activity_job_interview_preview);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
