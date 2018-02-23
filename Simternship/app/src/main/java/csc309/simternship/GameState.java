95f63b96e0762661b05681ebc4444275977f3129
Joel Dentici <joeldentici@cox.net>
2018-02-23 15:27:18 -0800

    Add game state, fixed #85

---
+++ Simternship/app/src/main/java/csc309/simternship/GameState.java
@@ -0,0 +1,34 @@
+package csc309.simternship;
+
+/**
+ * Created by joel on 2/23/18.
+ */
+
+public class GameState {
+    private static GameState gameState;
+
+    private String sharedState;
+
+    private GameState() {
+        sharedState = "Not Set Yet";
+    }
+
+    public static GameState getInstance() {
+        if (gameState == null) {
+            gameState = new GameState();
+        }
+
+        return gameState;
+    }
+
+    public void setState(String state) {
+        sharedState = state;
+    }
+
+    public String getState() {
+        return sharedState;
+    }
+
+
+
+}
