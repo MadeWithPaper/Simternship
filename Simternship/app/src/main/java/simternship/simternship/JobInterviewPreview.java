0869496ebf84c5b7a0de6c8abe02789d0b2a02fa
Dustyn Zierman-Felix <dziermanfelix@gmail.com>
2018-03-05 17:04:45 -0800

    #87 working on job interview/offer preview screens

--- Simternship/app/src/main/java/simternship/simternship/JobInterviewPreview.java
+++ Simternship/app/src/main/java/simternship/simternship/JobInterviewPreview.java
@@ -1,22 +1,16 @@
 package simternship.simternship;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
-
 import java.util.List;
 import java.util.ArrayList;
-
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.Arrays;
 
 public class JobInterviewPreview extends AppCompatActivity
 {
@@ -30,33 +24,25 @@ public class JobInterviewPreview extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         setup();
     }
 
-    private void setup() {
-        interviews = Arrays.asList(
-                new JobInterview("Google"),
-                new JobInterview("Apple")
-        );
+    private void setup()
+    {
+        reset();
+    }
 
+    public void reset()
+    {
+        interviews = GameState.getInstance().getCurrentJobInterviews();
         setItems(interviews);
     }
 
-
-    private void setItems(List<JobInterview> items) {
+    private void setItems(List<JobInterview> items)
+    {
         List<String> names = new ArrayList<>();
-        for (JobInterview item : items) {
+        for (JobInterview item : items)
+        {
             names.add(item.getCompanyName());
         }
 
@@ -75,19 +61,11 @@ public class JobInterviewPreview extends AppCompatActivity
         });
     }
 
-    private void goToInterview(int position) {
+    private void goToInterview(int position)
+    {
         JobInterview interview = interviews.get(position);
         Toast.makeText(this, interview.getCompanyName() + " clicked!", Toast.LENGTH_SHORT).show();
-
-    }
-
-    /*
-    Todo: Will have to fill the list with available interviews and use a switch statement to go to the
-        correct JobInterviewView
-     */
-    public void onClickInterview(View view)
-    {
-        Intent intent = new Intent(this, JobInterviewView.class);
-        startActivity(intent);
+        GameState.getInstance().setCurrentInterview(interview);
+        startActivity(new Intent(this, JobInterviewView.class));
     }
 }
--- Simternship/app/src/main/java/simternship/simternship/JobOfferPreview.java
+++ Simternship/app/src/main/java/simternship/simternship/JobOfferPreview.java
@@ -1,23 +1,15 @@
 package simternship.simternship;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
-
 import java.util.List;
 import java.util.ArrayList;
-
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.Arrays;
-import java.math.BigDecimal;
 
 public class JobOfferPreview extends AppCompatActivity
 {
@@ -31,32 +23,25 @@ public class JobOfferPreview extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         setup();
     }
 
-    private void setup() {
-        offers = Arrays.asList(
-                new JobOffer("Google", new BigDecimal("100000")),
-                new JobOffer("Apple", new BigDecimal("200000"))
-        );
+    private void setup()
+    {
+        reset();
+    }
 
+    public void reset()
+    {
+        offers = GameState.getInstance().getCurrentJobOffers();
         setItems(offers);
     }
 
-    private void setItems(List<JobOffer> items) {
+    private void setItems(List<JobOffer> items)
+    {
         List<String> names = new ArrayList<>();
-        for (JobOffer item : items) {
+        for (JobOffer item : items)
+        {
             names.add(item.getCompanyName());
         }
 
@@ -75,15 +60,10 @@ public class JobOfferPreview extends AppCompatActivity
         });
     }
 
-    private void goToOffer(int position) {
-        JobOffer offer = offers.get(position);
-        Toast.makeText(this, offer.getCompanyName() + " clicked!", Toast.LENGTH_SHORT).show();
-
-    }
-
-    public void onClickInterview(View view)
+    private void goToOffer(int position)
     {
-        Intent intent = new Intent(this, JobOfferView.class);
-        startActivity(intent);
+        JobOffer offer = offers.get(position);
+        GameState.getInstance().setCurrentOffer(offer);
+        startActivity(new Intent(this, JobOfferView.class));
     }
 }
