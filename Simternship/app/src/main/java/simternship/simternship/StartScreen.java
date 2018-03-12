015a780b90a439257e9e03bbf8898c66b9f24e4c
Jiaqing Mo <jimo@calpoly.edu>
2018-03-11 20:50:39 -0700

    #88

--- Simternship/app/src/main/java/simternship/simternship/StartScreen.java
+++ Simternship/app/src/main/java/simternship/simternship/StartScreen.java
@@ -2,7 +2,7 @@ package simternship.simternship;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class StartScreen extends AppCompatActivity {
+public class StartScreen extends Activity {
 
     EditText userEmail;
     EditText userPass;
