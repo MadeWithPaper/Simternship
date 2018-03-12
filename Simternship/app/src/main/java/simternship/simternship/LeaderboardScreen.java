38e6619b5748a93b6b16a2d64a8af18773dffe92
Jiaqing Mo <jimo@calpoly.edu>
2018-03-11 19:18:49 -0700

    #88

--- Simternship/app/src/main/java/simternship/simternship/LeaderboardScreen.java
+++ Simternship/app/src/main/java/simternship/simternship/LeaderboardScreen.java
@@ -1,5 +1,5 @@
 package simternship.simternship;
-
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -15,7 +15,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class LeaderboardScreen extends AppCompatActivity {
+//public class LeaderboardScreen extends AppCompatActivity {
+public class LeaderboardScreen extends Activity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Leaderboard");
     LinkedList<String> firstNames = new LinkedList<>();
