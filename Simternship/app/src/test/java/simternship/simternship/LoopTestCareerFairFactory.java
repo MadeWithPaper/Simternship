43e3e50ec496613ebbea32ae83e4781a4ec25a4a
Jiaqing Mo <jimo@calpoly.edu>
2018-03-13 19:05:56 -0700

    #94 loop testing for CareerFairFactory

---
+++ Simternship/app/src/test/java/simternship/simternship/LoopTestCareerFairFactory.java
@@ -0,0 +1,85 @@
+package simternship.simternship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by jiaqing on 3/13/18.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoopTestCareerFairFactory {
+
+    @Test
+    public void Test_createCareerFair0() {
+        RandomGenerator generator = mock(RandomGenerator.class);
+
+        CareerFairFactory factory = new CareerFairFactory(generator, 5, 10, 5, 10, Arrays.asList(
+                "Bob",
+                "Jim",
+                "Dave"
+        ));
+        List<Company> companies = new ArrayList<Company>(); //0 elements
+        CareerFair c = factory.createCareerFair(companies);
+        assertEquals(c.getBooths().size(),0);
+    }
+
+    @Test
+    public void Test_createCareerFair1() { //loop once
+        RandomGenerator generator = mock(RandomGenerator.class);
+
+        CareerFairFactory factory = new CareerFairFactory(generator, 5, 10, 5, 10, Arrays.asList(
+                "Bob",
+                "Jim",
+                "Dave"
+        ));
+        List<Company> companies = new ArrayList<Company>(); //0 elements
+        companies.add(new Company("Facebook",1, true, 1));
+        CareerFair c = factory.createCareerFair(companies);
+        assertEquals(c.getBooths().size(),1);
+    }
+
+    @Test
+    public void Test_createCareerFair2() { //loop twice
+        RandomGenerator generator = mock(RandomGenerator.class);
+
+        CareerFairFactory factory = new CareerFairFactory(generator, 5, 10, 5, 10, Arrays.asList(
+                "Bob",
+                "Jim",
+                "Dave"
+        ));
+        List<Company> companies = new ArrayList<Company>(); //0 elements
+        companies.add(new Company("Facebook",1, true, 1));
+        companies.add(new Company("Twitter",2, true, 2));
+        CareerFair c = factory.createCareerFair(companies);
+        assertEquals(c.getBooths().size(),2);
+    }
+
+    @Test
+    public void Test_createCareerFair3() { //loop 10 times
+        RandomGenerator generator = mock(RandomGenerator.class);
+
+        CareerFairFactory factory = new CareerFairFactory(generator, 5, 10, 5, 10, Arrays.asList(
+                "Bob",
+                "Jim",
+                "Dave"
+        ));
+        List<Company> companies = new ArrayList<Company>(); //0 elements
+        Company c = new Company("Facebook",1, true, 1);
+        for (int i = 1; i<=10; i++){
+            companies.add(c);
+        }
+        CareerFair cf = factory.createCareerFair(companies);
+        assertEquals(cf.getBooths().size(),10);
+    }
+
+}
